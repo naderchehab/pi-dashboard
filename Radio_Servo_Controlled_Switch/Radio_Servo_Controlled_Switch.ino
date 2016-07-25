1a2aa8d72606e2bdeca80615738b103cad82a1e0
Nader Chehab <naderchehab@gmail.com>
2016-07-24 17:31:25 -0700

    Code cleanup

--- Radio_Servo_Controlled_Switch/Radio_Servo_Controlled_Switch.ino
+++ Radio_Servo_Controlled_Switch/Radio_Servo_Controlled_Switch.ino
@@ -1,52 +1,54 @@
-// When a specific code is received over radio, the arduino triggers the light servo to control the light switch.
+// When a specific code is received over radio (in this case the code is 22275), the arduino rotates the servo which flips the light switch on or off.
+// Original code is from carjo3000 on thingiverse: http://www.thingiverse.com/thing:1156995. I modified it to suit my requirements.
 
 #include <Servo.h>
 
 #include <RCSwitch.h>
 
 RCSwitch mySwitch = RCSwitch();
-
 Servo myservo;  // create servo object to control a servo
-
 int pos = 0;    // variable to store the servo position
-
 boolean isLightOn = true;
 
 void setup()
 {
-  myservo.attach(9);  // attaches the servo to pin 9 of arduino
-  mySwitch.enableReceive(0);  // Receiver on inerrupt 0 => that is pin #2
+  myservo.attach(9);  // attaches the servo to pin #9 of arduino
+  mySwitch.enableReceive(0);  // attaches the radio receiver to inerrupt 0 => that is pin #2
   Serial.begin(9600);
 }
 
 void turnOn() {
-  for (pos = 60; pos < 100; pos += 1) // goes from 60 degrees to 100 degrees
-  { // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+  // To turn the lights on, the servo goes from 60 degrees to 100 degrees. In this case, 60 degrees is the "neutral" position and 100 degrees is the "on" position.
+  for (pos = 60; pos < 100; pos += 1)
+  {
+    myservo.write(pos);
     delay(15);                       // waits 15ms for the servo to reach the position
   }
-  for (pos = 100; pos >= 60; pos -= 1) // goes from 100 degrees to 60 degrees
+
+  // After the switch is flipped, go back to neutral position
+  for (pos = 100; pos >= 60; pos -= 1)
   {
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    myservo.write(pos);
+    delay(15);
   }
   isLightOn = true;
 }
 
 void turnOff() {
-  for (pos = 60; pos >= 10; pos -= 1) // goes from 60 degrees to 5 degrees
+  for (pos = 60; pos >= 10; pos -= 1)
   {
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    myservo.write(pos);
+    delay(15);
   }
 
-  for (pos = 10; pos < 60; pos += 1) // goes from 5 degrees to 60 degrees
-  { // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+  for (pos = 10; pos < 60; pos += 1)
+  {
+    myservo.write(pos);
+    delay(15);
   }
   isLightOn = false;
 }
+
 void loop()
 {
   if (mySwitch.available()) {
@@ -63,7 +65,7 @@ void loop()
       Serial.print("bit ");
       Serial.print("Protocol: ");
       Serial.println( mySwitch.getReceivedProtocol() );
-      
+
       if (value == 22275) {
         if (isLightOn == true) {
           turnOff();
@@ -77,5 +79,3 @@ void loop()
     mySwitch.resetAvailable();
   }
 }
-
-
